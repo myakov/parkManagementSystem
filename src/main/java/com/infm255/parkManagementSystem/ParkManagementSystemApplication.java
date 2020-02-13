2de7ee670a7c56af5d6166a4178beb54fd8ab8c1
milen.yakov <milen.yakov@egt-bg.com>
2020-02-13 15:20:32 +0200

    Initial commit

---
+++ src/main/java/com/infm255/parkManagementSystem/ParkManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.infm255.parkManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkManagementSystemApplication.class, args);
+	}
+
+}
